7b63d8fa60a9e3aae3101f391e256b206a2ed47f
Gunawan Nur Ahmad <54525132+GunawanAhmad@users.noreply.github.com>
2024-06-16 15:31:08 +0800

    update: change float terminal binding

--- .config/nvim/lua/plugins/float-term.lua
+++ .config/nvim/lua/plugins/float-term.lua
@@ -1,7 +1,7 @@
 return {
   "voldikss/vim-floaterm",
   config = function()
-    vim.keymap.set("n", "<C-`>", ":FloatermToggle <CR>")
-    vim.keymap.set("t", "<C-`>", "<C-\\><C-n>:FloatermToggle <CR>")
+    vim.keymap.set("n", "<C-t>", ":FloatermToggle <CR>")
+    vim.keymap.set("t", "<C-t>", "<C-\\><C-n>:FloatermToggle <CR>")
   end,
 }
