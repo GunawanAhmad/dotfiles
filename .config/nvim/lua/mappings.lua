6dc238cff551e34e4593adad9660bb261af3074c
Gunawan Nur Ahmad <54525132+GunawanAhmad@users.noreply.github.com>
2024-11-14 14:24:00 +0800

    fix mapping and styling nvim

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,39 +1,26 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "jk", "<ESC>", { desc = "Back to normal mode" })
 
 -- remap for halp page up and down
-map("n", "K", "<C-u>")
-map("n", "J", "<C-d>")
+map("n", "K", "<C-u>", { desc = "Page up" })
+map("n", "J", "<C-d>", { desc = "Page down" })
 
-map("v", "J", ":m '>+1<CR>gv=gv")
-map("v", "K", ":m '<-2<CR>gv=gv")
-map("n", "esc", ":noh<return><esc>")
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 
 -- Copy to system clipboard
-map("v", "<leader>y", '"+y')
-map("n", "<leader>y", '"+y')
-map("v", "<leader>Y", '"+Y')
+map("v", "<leader>y", '"+y', { desc = "Copy" })
+map("n", "<leader>y", '"+y', { desc = "Copy" })
+map("v", "<leader>Y", '"+Y', { desc = "Copy" })
 
-map("n", "X", "<C-w>")
+map("n", "X", "<C-w>", { desc = "Navigation prefix" })
 
--- Remap new arrow key to hjkl
-map("i", "<C-h>", "<left>")
-map("i", "<C-l>", "<right>")
-map("i", "<C-j>", "<down>")
-map("i", "<C-k>", "<up>")
-map("n", "<C-h>", "<left>")
-map("n", "<C-l>", "<right>")
-map("n", "<C-j>", "<down>")
-map("n", "<C-k>", "<up>")
-map("v", "<C-h>", "<left>")
-map("v", "<C-l>", "<right>")
-map("v", "<C-j>", "<down>")
-map("v", "<C-k>", "<up>")
+map({ "n", "t" }, "<C-t>", function()
+  require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
+end, { desc = "terminal toggleable horizontal term" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
