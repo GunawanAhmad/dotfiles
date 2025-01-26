d74991c050ee7fc90fb68055e7b52c7e22cf717b
Gunawan Nur Ahmad <54525132+GunawanAhmad@users.noreply.github.com>
2025-01-27 02:11:59 +0800

    fix statusline

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -60,6 +60,8 @@ vim.opt.scrolloff = 10
 vim.opt.softtabstop = 4
 vim.opt.tabstop = 4
 
+vim.opt.laststatus = 4
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,12 +2,33 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    options = {
-      theme = 'gruvbox-material',
-      extensions = {
-        'nvim-tree',
+    require('lualine').setup {
+      icons_enabled = true,
+      theme = 'auto',
+      options = {
+        theme = 'auto',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'searchcount', 'diagnostics', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      extensions = { 'nvim-tree' },
+      disabled_filetypes = {
+        'NvimTree',
       },
     }
-    require('lualine').setup()
   end,
 }
