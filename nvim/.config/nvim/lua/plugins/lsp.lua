cc044679dad327cb31e551e8724bad85f498d9e6
Gunawan Nur Ahmad <gunawanahmadef@gmail.com>
2025-07-30 01:37:09 +0800

    add: supermaven

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -154,9 +154,6 @@ return {
       rust_analyzer = {},
       golangci_lint_ls = {
        filetypes = { 'go', 'gomod' },
-        init_options = {
-          command = { 'golangci-lint', 'run', '--output.json.path=stdout', '--show-stats=false' },
-        },
       },
       -- This is removed because we handle ts lsp with volar
       -- ts_ls = {
